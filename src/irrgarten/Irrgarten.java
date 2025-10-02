1c0f8f1104fa0ea9ceaa77c0febf45142e458a4c
AsierPerales <asierperalesna@correo.ugr.es>
2025-10-02 18:18:12 +0200

    TestP1 hecho :)

--- src/irrgarten/Irrgarten.java
+++ src/irrgarten/Irrgarten.java
@@ -13,18 +13,6 @@ public class Irrgarten {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Weapon arma = new Weapon(3,3);
-        Shield escudin = new Shield(4,1);
-        Dice dadin = new Dice();
-        
-        arma.attack();
-        escudin.protect();
-        
-        System.out.println(arma.toString());
-        System.out.println(escudin.toString());
-        System.out.print(dadin.randomPos(5));
-    }
+//    
     
 }
