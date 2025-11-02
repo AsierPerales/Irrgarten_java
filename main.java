0ef165cfdc0e92815d44ecaef565a87a4147fb72
AsierPerales <asierperalesna@correo.ugr.es>
2025-11-02 15:11:36 +0100

    Arreglada estructura...Listo para P4?

---
+++ main.java
@@ -0,0 +1,35 @@
+package irrgarten;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import irrgartenController.Controller;
+
+import irrgartenTextUI.TextUI;
+
+/**
+ *
+ * @author asier
+ */
+public class main {
+    public static void main(String[] args){
+       
+      // Inicializamos vista  y controlador
+      TextUI vista  = new TextUI();                        
+      Controller controlador;
+      
+      // Inicializamos modelo
+      Game juego;
+      juego = new Game(2);
+      
+      // Llamamos a play
+      controlador = new Controller(juego,vista);
+      controlador.play();    
+        
+    }
+}
--- src/irrgarten/main.java
+++ src/irrgarten/main.java
@@ -1,8 +1,12 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package irrgartenGame;
+
+package irrgarten;
+
+import irrgartenGame.Game;
 
 import irrgartenController.Controller;
 
